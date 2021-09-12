09c64b886f05cf12d541e725d4daf5fd12adae47
khushxxl <khushaal.choithramani@gmail.com>
2021-09-12 11:42:49 +0530

    update v4

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ import FacebookProvider from "next-auth/providers/facebook";
 export default NextAuth({
   providers: [
     FacebookProvider({
-      clientId: "2903859583197046",
-      clientSecret: "717b83b40a88f5c76e16a7b88e6a3d6f",
+      clientId: "256966142819666",
+      clientSecret: "fe37b85048740c31b79ebef59d042cc3",
     }),
   ],
 });
